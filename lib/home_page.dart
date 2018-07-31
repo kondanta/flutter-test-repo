51351f2bbcb540f62389844178cc0c84732b9bee
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-07-31 14:17:43 +0300

    Grid view aspect ratio Fix

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -44,9 +44,10 @@ class _HomePageState extends State<HomePage> {
           child: _isLoading
               ? new CircularProgressIndicator()
               : GridView.count(
-                  crossAxisCount: 2,
+                  crossAxisCount: 3,
                   padding: EdgeInsets.all(16.0),
-                  childAspectRatio: 8.0 / 9.0,
+                  childAspectRatio: 5.0 / 9.0,
+                  shrinkWrap: true,
                   children: _buildGridCards(context, lst) // Changed code
                   ),
         ));
@@ -77,79 +78,6 @@ class DetailsPage extends StatelessWidget {
   }
 }
 
-class MangaRow extends StatelessWidget {
-  final manga;
-  MangaRow({this.manga});
-
-  @override
-  Widget build(BuildContext context) {
-    // Manga thumbnail variable
-    final mangaThumbnail = new Container(
-        margin: EdgeInsets.symmetric(vertical: 0.0),
-        alignment: FractionalOffset.topLeft,
-        child: Image.network(manga['img'], height: 100.0, width: 120.0));
-
-    // Manga Card Details
-    final mangaDetails = new Container(
-      margin: new EdgeInsets.fromLTRB(76.0, 16.0, 16.0, 16.0),
-      constraints: new BoxConstraints.expand(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Container(height: 8.0),
-          new Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Text(
-                manga['name'],
-                style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w600), //textstyle
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-              ),
-            ],
-          ), //text
-        ], //widget[]
-      ), //column
-    );
-
-    // Manga card variable
-    final mangaCard = new Container(
-      child: mangaDetails,
-      height: 100.0,
-      margin: EdgeInsets.only(left: 46.0),
-      decoration: BoxDecoration(
-        color: Color(0xFF333366),
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.circular(8.0),
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-            color: Colors.black12,
-            blurRadius: 10.0,
-            offset: Offset(0.0, 10.0),
-          ),
-        ],
-      ), //box decoration
-    );
-
-    // Renderer
-    return Container(
-      margin: const EdgeInsets.symmetric(
-        vertical: 16.0,
-        horizontal: 24.0,
-      ), //EdgeInsets,
-      child: Stack(
-        children: <Widget>[
-          mangaCard,
-          mangaThumbnail,
-        ], //Widget[]
-      ), //Stack
-    ); //container;
-  }
-}
-
 List<Card> _buildGridCards(BuildContext context, List products) {
   if (products == null || products.isEmpty) {
     return const <Card>[];
@@ -163,9 +91,10 @@ List<Card> _buildGridCards(BuildContext context, List products) {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           AspectRatio(
-            aspectRatio: 18 / 11,
+            aspectRatio: 7 / 9,
             child: Image.network(
               product['img'],
+              fit: BoxFit.fill,
               // TODO: Adjust the box size (102)
             ),
           ),
@@ -180,9 +109,9 @@ List<Card> _buildGridCards(BuildContext context, List products) {
                   Text(
                     product['name'],
                     // style: theme.textTheme.title,
-                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
                   ),
-                  SizedBox(height: 8.0),
                 ],
               ),
             ),
